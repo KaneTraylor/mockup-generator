414f4c20916db8aa34ea41281174383d21ed0688
Kane Traylor <97408648+KaneTraylor@users.noreply.github.com>
2023-01-15 17:20:41 -0500

    Add files via upload

---
+++ Mock up generator/assets/js/index.js
@@ -0,0 +1,14 @@
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _lib_recurly_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./lib/recurly.css */ "./lib/recurly.css");
+/* harmony import */ var _lib_recurly_css__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_lib_recurly_css__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _lib_recurly__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./lib/recurly */ "./lib/recurly.js");
+/**
+ * License information available at https://docs.recurly.com/page/open-source-attribution
+ * @attribution
+ */
+
+
+var recurly = new _lib_recurly__WEBPACK_IMPORTED_MODULE_1__["Recurly"]();
+/* harmony default export */ __webpack_exports__["default"] = (recurly);
+
+//# sourceURL=webpack://recurly/./index.js?
